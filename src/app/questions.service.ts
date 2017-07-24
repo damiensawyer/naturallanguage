dfd92087c9d516213882648a834badbb9c501c8b
Damien Sawyer <damiensawyer@gmail.com>
2017-07-24 12:58:16 +0100

    questions

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -10,9 +10,8 @@ export class QuestionsService {
       Question.Create(1, 'Navigate to POS'),
       Question.Create(2, 'Navigate to Home'),
       Question.Create(3, 'Navigate to Back'),
-      Question.Create(4, 'Navigate to Front'),
-      Question.Create(5, 'Navigate to Door'),
-      Question.Create(6, 'Navigate to Chicken')
+      Question.Create(4, 'Are There any bookings for Jones'),
+      Question.Create(5, `Show me a report of today's bookings`)
     ];
   }
 
