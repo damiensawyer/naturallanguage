c65627403d441e517009690e8367c4e6ac350343
Damien Sawyer <damiensawyer@gmail.com>
2017-08-09 21:55:30 +0100

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
-import { QuestionsService } from './questions.service';
-import { FakeLanguageParseServiceService } from './fake-language-parse-service.service';
-import { LUISLanguageParseService } from './luislanguage-parse.service';
 import { Injectable } from '@angular/core';
-import { Question } from './question';
 import { Transition, Ng2StateDeclaration, UIView } from '@uirouter/angular';
-import * as r from '@uirouter/angular';
 
 @Component({
   selector: 'app-root',
